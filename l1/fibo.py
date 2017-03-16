15c2a132d4959a674468f1fa8214499123676bca
linafru <linafruscianta@gmail.com>
2017-03-16 11:19:00 +0300

    Python module example

---
+++ l1/fibo.py
@@ -0,0 +1,10 @@
+# Fibonacci numbers module
+
+def fib_func(n):    # write Fibonacci series up to n
+    """Print a Fibonacci series up to n.
+    """
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
