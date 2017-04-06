c22a36457f0bf2ac5d07550a17974ef02bf646a5
linafru <linafruscianta@gmail.com>
2017-04-06 11:26:38 +0300

    geospatial tutorial

---
+++ cheatsheets/example_module.py
@@ -0,0 +1,8 @@
+
+
+example_variable =' I am an example variable!'
+
+
+def example_function():
+    print('I am gonna print example variable!!!!')
+    print(example_variable)
---
+++ cheatsheets/pandas.md
@@ -0,0 +1,5 @@
+pandas.md
+
+
+## Links
+[One more cheatsheet](https://github.com/pandas-dev/pandas/blob/master/doc/cheatsheet/Pandas_Cheat_Sheet.pdf)
